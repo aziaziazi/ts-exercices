67725d018b20f72664e5918d21ab5c38deeab895
Camille Gabrieli <aziaziazi@gmail.com>
2026-04-02 09:27:15 +0200

    Clarify comment

--- src/arrays/arrays.ts
+++ src/arrays/arrays.ts
@@ -181,7 +181,7 @@ export const groupByVowelCount = (
  *
  *
  * Use : [...Array()), Array.keys(), map, Math.floor(), Math.sign
- * Or while() and none of the above
+ * Or use while() and none of the above methods
  */
 export const range = (_start: number, _end: number, _step = 1): number[] => {
   return []
